5cd4bdc22ee987a67fcb7d3e5f23d4a3fca38a1a
Wanda White <wanda.white32@yahoo.com>
2021-07-26 13:09:56 -0500

    removing unnecessary comments

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -723,10 +723,4 @@ const actionCreators = {
 }
       
 export default connect(mapState, actionCreators)(Dashboard);
-// Select a Category to view its questions
 
-// -Category 1 BMW Int3 //Onclick expand list of questions and total answers
-// -Category 2 Chevrolet Bolt //Onclick
-// -Category 3 Kia Soul //Onclick
-// -Category 4 Nissan Leaf //Onclick
-// -Category 5 Tesla Model 3 //Onclick
